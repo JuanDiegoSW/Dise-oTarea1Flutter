401179cac4d1235abb1db0726cf4abcc32156969
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2021-11-01 20:19:40 -0500

    update before

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-// Uncomment lines 7 and 10 to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-
 void main() {
-  // debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
